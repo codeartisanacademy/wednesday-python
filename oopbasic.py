35ea4c8986864d128c8722d71879d244bc5ca4d2
wahyudi <wahyudi.coding@gmail.com>
2021-04-28 16:53:54 +0700

    oop

---
+++ oopbasic.py
@@ -0,0 +1,19 @@
+class Student():
+
+    # constructor
+    def __init__(self, name, birthday, faculty):
+        self.name = name
+        self.birthday = birthday
+        self.faculty = faculty
+    
+    def attend_class(self, subject):
+        print("{0} is attending {1} class".format(self.name, subject))
+
+john = Student(name="John", birthday="26/04/2000", faculty="Science")
+dewi = Student(name="Dewi", birthday="29/05/1998", faculty="Computer science")
+
+print(john.name)
+print(dewi.birthday)
+
+john.attend_class("Python for data")
+dewi.attend_class("Photography")
